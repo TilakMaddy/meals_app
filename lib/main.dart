e0ef348fff5c04a050c780ef036b44f1c20c2e08
TilakMaddy <tilak.madichetti@gmail.com>
2021-06-23 00:28:59 +0530

    initial with catergory items

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/categories_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meals App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meals App"),
+      ),
+      body: Center(
+        child: Text('Navigation time !'),
+      ),
+    );
+  }
+}
